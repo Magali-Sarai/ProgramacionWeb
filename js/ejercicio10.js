9b0595ffefdf2b0281bb7034315bb27f5616f25b
Magali <23160887@itoaxaca.edu.mx>
2026-09-14 19:19:01 -0600

    primeros 10 jiji

---
+++ js/ejercicio10.js
@@ -0,0 +1,13 @@
+function convertir(){
+    //Obtiene el valor del los grados
+    var grados=document.getElementById("grados").value;
+    //Validación
+    if(grados==""){
+        alert("Pon algo carck");
+        return;
+        }
+    //Convierte 
+    var resultado=(parseInt(grados)*1.8)+32;
+    //Muestra el rsultado en el elemento con id resultado
+    document.getElementById("resultado").value = resultado + " F";
+}
